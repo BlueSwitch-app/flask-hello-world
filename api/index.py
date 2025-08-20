9fad5448579978b82e02fa6a47b9619c761c8a83
Cristopher <150216503+Cristopher-a@users.noreply.github.com>
2025-08-20 10:52:17 -0600

    .

--- api/index.py
+++ api/index.py
@@ -20,6 +20,6 @@ def connect():
 # Send a ping to confirm a successful connection
     try:
         client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
+        return ("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
-        print(e)
+        return (e)
