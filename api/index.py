e9479b39f0624a90894c21e23e6382ac11eed4fd
Cristopher <150216503+Cristopher-a@users.noreply.github.com>
2025-08-20 10:48:45 -0600

    .

--- api/index.py
+++ api/index.py
@@ -1,5 +1,8 @@
 from flask import Flask
-
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://crisesv4:Tanke280423@cluster0.ejxv3jy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri, server_api=ServerApi('1'))
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,3 +12,14 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route("/Connection")
+def connect():
+    # Create a new client and connect to the server
+    
+# Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
